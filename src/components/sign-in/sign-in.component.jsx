f2afbc9e2c1525716afa8ceefc7c6118069875e1
xiaomingniu129 <32426814+xiaomingniu129@users.noreply.github.com>
2020-01-22 04:43:05 -0800

    stripe payment method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -19,8 +19,9 @@ class SignIn extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-    
-    const { email, password} = this.state;
+
+    const { email, password } = this.state;
+
     try {
       await auth.signInWithEmailAndPassword(email, password);
       this.setState({ email: '', password: '' });
